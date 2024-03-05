433ac587fdd56871290c4bf7f54adf0f285a4c38
caomainhau <manhhaucao@gmail.com>
2024-03-05 22:48:53 +0700

    123

---
+++ src/IncrementThread.java
@@ -0,0 +1,33 @@
+
+public class IncrementThread extends Thread {
+
+    private Counter counter;
+
+    private int incrementsPerThread;
+
+    public IncrementThread(Counter counter, int incrementsPerThread) {
+
+        this.counter = counter;
+
+        this.incrementsPerThread = incrementsPerThread;
+
+    }
+
+    public void run() {
+
+        for (int i = 0; i < incrementsPerThread; i++) {
+
+            try {
+
+                counter.increment();
+
+            } catch (InterruptedException e) {
+
+                throw new RuntimeException(e);
+
+            }
+
+        }
+
+    }
+}
