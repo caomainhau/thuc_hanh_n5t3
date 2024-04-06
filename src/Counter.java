5dd165d98432c7836cd01a2d2cc7ab94f894854c
caomainhau <manhhaucao@gmail.com>
2024-04-06 21:35:05 +0700

    update

--- src/Counter.java
+++ src/Counter.java
@@ -1,28 +1,16 @@
-
- public class Counter {
-
-
+public class Counter {
 
     private int count = 0;
 
-
-
+    // Increment the count by 1 and wait for 1000 milliseconds (1 second)
     public synchronized void increment() throws InterruptedException {
-
         count++;
-
         System.out.print("" + count + " ");
-
-                Thread.sleep(1000);
-
+        Thread.sleep(1000);
     }
 
-
-
+    // Get the current count value
     public int getCount() {
-
         return count;
-
     }
-
 }
--- src/IncrementThread.java
+++ src/IncrementThread.java
@@ -1,33 +1,25 @@
-
 public class IncrementThread extends Thread {
 
     private Counter counter;
-
     private int incrementsPerThread;
 
+    // Constructor to initialize IncrementThread with a Counter instance and the number of increments per thread
     public IncrementThread(Counter counter, int incrementsPerThread) {
-
         this.counter = counter;
-
         this.incrementsPerThread = incrementsPerThread;
-
     }
 
+    // Run method to execute the thread
     public void run() {
-
+        // Loop to perform the specified number of increments
         for (int i = 0; i < incrementsPerThread; i++) {
-
             try {
-
+                // Call the increment method of the Counter instance
                 counter.increment();
-
             } catch (InterruptedException e) {
-
+                // If an InterruptedException occurs, wrap it in a RuntimeException and throw
                 throw new RuntimeException(e);
-
             }
-
         }
-
     }
 }
